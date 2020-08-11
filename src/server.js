78f347b62feab9c3a202db28997dbf12dc063cb5
Luan Queiroz <luan.queiroz2003@gmail.com>
2020-08-11 16:25:05 -0300

    Primeiro Commit

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const dataBase = require('sqlite-async')
+
+// server
+const express = require('express')
+const server = express()
+
+const {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require('./pages')
+
+// config nunjucks (template engine)
+const nunjucks = require('nunjucks') 
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
+
+// inicio e config do server
+server
+
+// receber os dados do req.body
+.use(express.urlencoded({ extended: true }))
+// config arquivos estáticos (imgs, css, scripts, etc)
+.use(express.static("public"))
+
+// rotas da aplicação
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
+
+.listen(5500)
