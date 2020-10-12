c24e8337bc7364f56245f9e1272d05bb9f8840da
Luan Queiroz <luan.queiroz2003@gmail.com>
2020-10-12 17:55:56 -0300

    save Proffy
    Signed-off-by: Luan Queiroz <luan.queiroz2003@gmail.com>

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles/global.css';
+// JSX = JavaScript XML
+
+import Routes from './routes';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
